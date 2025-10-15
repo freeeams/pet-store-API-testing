5ed351af03bad29fcf2e3f709ee4947555555069
freeeams <freams.fox@gmail.com>
2025-10-14 20:38:09 -0500

    deleteUser

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -6,7 +6,7 @@ test.describe('User API Tests', () => {
     test('Create a new user', async ({ request }) => {
         const createUserRequestBody = {
             "id": 12312,
-            "username": "TestUserNameSalih123",
+            "username": "TestUserName-test-automation-delete-me",
             "firstName": faker.person.firstName(),
             "lastName": faker.person.lastName(),
             "email": faker.internet.email(),
@@ -26,23 +26,41 @@ test.describe('User API Tests', () => {
         const actualResponseBody = await createUserResponse.json();
         expectedResponseSchema.parse(actualResponseBody);
     });
+    test('get user by username', async ({ request }) => {
+        const username = "TestUserNameSalih123";
+        let getUserResponse;
+        for (let i = 0; i < 5; i++) {
+            getUserResponse = await request.get(`${BASE_URL}/user/${username}`);
+            if(getUserResponse.status() === 200) {
+                break; // Exit loop if the request is successful
+            }
+            console.log(`Attempt ${i + 1} failed. Retrying...`);
+        }
+        expect(getUserResponse.status()).toBe(200);
+        const expectedResponseSchema = z.object({
+            id: z.number(),
+            username: z.literal(username),
+            firstName: z.string(),
+            lastName: z.string(),
+            email: z.string().email(),
+            password: z.string(),
+            phone: z.string(),
+            userStatus: z.number(),
+        })
+        const actualResponseBody = await getUserResponse.json();
+        expectedResponseSchema.parse(actualResponseBody);
+    });
+    test('Delete user by username', async ({ request }) => {
+        const username = "TestUserName-test-automation-delete-me";
+        const deleteUserResponse = await request.delete(`${BASE_URL}/user/${username}`);
+        expect(deleteUserResponse.status()).toBe(200);
+        const expectedResponseSchema = z.object({
+            code: z.literal(200),
+            type: z.literal("unknown"),
+            message: z.literal(username),
+        })
+        const actualResponseBody = await deleteUserResponse.json();
+        expectedResponseSchema.parse(actualResponseBody);
+    });
+});
 
-});
-test('get user by username', async ({ request }) => {
-    const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
-    const username = "TestUserNameSalih123";
-    const getUserResponse = await request.get(`${BASE_URL}/user/${username}`);
-    expect(getUserResponse.status()).toBe(200);
-    const expectedResponseSchema = z.object({
-        id: z.number(),
-        username: z.literal(username),
-        firstName: z.string(),
-        lastName: z.string(),
-        email: z.string().email(),
-        password: z.string(),
-        phone: z.string(),
-        userStatus: z.number(),
-    })
-    const actualResponseBody = await getUserResponse.json();
-    expectedResponseSchema.parse(actualResponseBody);
-});
